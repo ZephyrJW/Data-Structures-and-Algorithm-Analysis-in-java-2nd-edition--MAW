da7a8b266567960b045e71a61dc402f0ff9a0dbe
Jiawei Zhao <jiawei_zhao@outlook.com>
2016-02-10 19:10:04 -0800

    Update BinarySearchTree.java

--- Chapter4-Tree/BinarySearchTree.java
+++ Chapter4-Tree/BinarySearchTree.java
@@ -31,7 +31,51 @@ public class BinarySearchTree<AnyType> extends Comparable<super AnyType>{
     return root == null;
   }
   
-  public bollean contains(AnyType x){
+  public boolean contains(AnyType x){
     return contains(x, root);
+  }
+  
+  public AnyType findMin(){
+    if(isEmpty()) return UnderflowException();
+    return findMin(root).element;
+  }
+  public AnyType findMax(){
+    if(isEmpty()) return UnderflowException();
+    return findMax(root).element;
+  }
+  
+  public void insert(AnyType x){
+    root = insert(x, root);
+  }
+  public void remove(AnyType x){
+    root = remove(x, root)
+  }
+  
+  public void printTree(){
+    
+  }
+  
+  private boolean contains(AnyType x, BinaryNode<AnyType> t){
+    
+  }
+  
+  private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t){
+    
+  }
+  
+  private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t){
+    
+  }
+  
+  private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t){
+    
+  }
+  
+  private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t){
+    
+  }
+  
+  private void printTree(BinaryNode<AnyType> t){
+    
   }
 }
